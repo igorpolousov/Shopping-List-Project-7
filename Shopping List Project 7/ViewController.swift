7772f37e3324c35e29713d52765a3f8a3deab2e7
Igor Polousov <igorpolousov@gmail.com>
2021-07-21 15:25:35 +0300

    Initial Commit

---
+++ Shopping List Project 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping List Project 7
+//
+//  Created by Igor Polousov on 21.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
